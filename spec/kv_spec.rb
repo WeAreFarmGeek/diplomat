719aeccc5d923642ca8837bdbb04cfe5acab86e4
Sumit Vij <sumit.vij@lookout.com>
2015-02-13 18:03:20 -0800

    Add CAS support

--- spec/kv_spec.rb
+++ spec/kv_spec.rb
@@ -10,6 +10,7 @@ describe Diplomat::Kv do
     let(:key) { "key" }
     let(:key_url) { "/v1/kv/#{key}" }
     let(:key_params) { "toast" }
+    let(:modify_index) { 99 }
 
     it "GET" do
       json = JSON.generate([{
@@ -26,28 +27,22 @@ describe Diplomat::Kv do
     end
 
     it "PUT" do
-
-      json = JSON.generate([{
-        "Key"   => key,
-        "Value" => Base64.encode64(key_params),
-        "Flags" => 0
-      }])
-
-      faraday.stub(:get).and_return(OpenStruct.new({ body: json }))
-      faraday.stub(:put).and_return(OpenStruct.new({ body: "true\n"}))
+      faraday.stub(:put).with(key_url, key_params).and_return(OpenStruct.new({ body: "true\n"}))
       kv = Diplomat::Kv.new(faraday)
 
       expect(kv.put(key, key_params)).to eq(key_params)
     end
 
-    it "namespaces" do
-      json = JSON.generate([{
-        "Key"   => key,
-        "Value" => Base64.encode64(key_params),
-        "Flags" => 0
-      }])
+    it "cas PUT" do
+      options = {:cas => modify_index}
+      url = "#{key_url}?cas=#{modify_index}"
+      faraday.stub(:put).with(url, key_params).and_return(OpenStruct.new({ body: "true\n"}))
 
-      faraday.stub(:get).and_return(OpenStruct.new({ body: json }))
+      kv = Diplomat::Kv.new(faraday)
+      expect(kv.put(key, key_params, options)).to eq(key_params)
+    end
+
+    it "namespaces" do
       faraday.stub(:put).and_return(OpenStruct.new({ body: "true\n"}))
       kv = Diplomat::Kv.new(faraday)
 
