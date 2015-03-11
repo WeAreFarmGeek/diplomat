ed825dcffa88223902e6326c68d89b64de7e5f2e
Matt Turner <matturner@gmail.com>
2015-03-11 21:42:36 +0000

    Session arguments are optional.

--- lib/diplomat/session.rb
+++ lib/diplomat/session.rb
@@ -6,12 +6,12 @@ module Diplomat
     # Create a new session
     # @param value [Object] hash or json representation of the session arguments
     # @return [String] The sesssion id
-    def create value
+    def create value=nil
       # TODO: only certain keys are recognised in a session create request,
       # should raise an error on others.
       raw = @conn.put do |req|
         req.url "/v1/session/create"
-        req.body = if value.kind_of?(String) then value else JSON.generate(value) end
+        req.body = (if value.kind_of?(String) then value else JSON.generate(value) end) unless value.nil?
       end
       body = JSON.parse(raw.body)
       return body["ID"]
