cba78c6b4ccefaac1b7e5d19e188f73b44628e25
Matt Turner <matturner@gmail.com>
2015-03-23 22:14:28 +0000

    No need to assign this value to the api_connection parameter.

--- lib/diplomat/rest_client.rb
+++ lib/diplomat/rest_client.rb
@@ -21,7 +21,7 @@ module Diplomat
     # Build the API Client
     # @param api_connection [Faraday::Connection,nil] supply mock API Connection
     def start_connection api_connection=nil
-      @conn = api_connection ||= Faraday.new(:url => Diplomat.configuration.url) do |faraday|
+      @conn = api_connection || Faraday.new(:url => Diplomat.configuration.url) do |faraday|
         faraday.request  :url_encoded
         Diplomat.configuration.middleware.each do |middleware|
           faraday.use middleware
