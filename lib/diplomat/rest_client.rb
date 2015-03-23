03eb4a694d14d14106c3b299a21b6130005f9608
Matt Turner <matturner@gmail.com>
2015-03-23 22:15:03 +0000

    Re-arrange to make more sense, and use symbols to look up middleware

--- lib/diplomat/rest_client.rb
+++ lib/diplomat/rest_client.rb
@@ -22,12 +22,13 @@ module Diplomat
     # @param api_connection [Faraday::Connection,nil] supply mock API Connection
     def start_connection api_connection=nil
       @conn = api_connection || Faraday.new(:url => Diplomat.configuration.url) do |faraday|
+        faraday.adapter  Faraday.default_adapter
         faraday.request  :url_encoded
+        faraday.response :raise_error
+
         Diplomat.configuration.middleware.each do |middleware|
           faraday.use middleware
         end
-        faraday.adapter  Faraday.default_adapter
-        faraday.use      Faraday::Response::RaiseError
       end
     end
 
