210ca232d43592660250c68e3de802d264f6da5d
John Hamelink <hello@farmer.io>
2015-05-07 19:34:41 +0100

    Extract connection building

--- lib/diplomat/rest_client.rb
+++ lib/diplomat/rest_client.rb
@@ -25,19 +25,15 @@ module Diplomat
     # Build the API Client
     # @param api_connection [Faraday::Connection,nil] supply mock API Connection
     def start_connection api_connection=nil
-      @conn = api_connection || Faraday.new(:url => Diplomat.configuration.url) do |faraday|
+      @conn = build_connection(api_connection)
+      @conn_no_err = build_connection(api_connection, true)
+    end
+
+    def build_connection(api_connection, raise_error=false)
+      return api_connection || Faraday.new(:url => Diplomat.configuration.url) do |faraday|
         faraday.adapter  Faraday.default_adapter
         faraday.request  :url_encoded
-        faraday.response :raise_error
-
-        Diplomat.configuration.middleware.each do |middleware|
-          faraday.use middleware
-        end
-      end
-
-      @conn_no_err = api_connection || Faraday.new(:url => Diplomat.configuration.url) do |faraday|
-        faraday.adapter  Faraday.default_adapter
-        faraday.request  :url_encoded
+        faraday.response :raise_error if raise_error
 
         Diplomat.configuration.middleware.each do |middleware|
           faraday.use middleware
