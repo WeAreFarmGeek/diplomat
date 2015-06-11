9995b927a81a634389f88ac6564916efb79cc33e
Toby Jackson <toby.jackson@futurenet.com>
2015-06-11 12:30:43 +0100

    Added datacenter option to get a list of avaliable datacenters

---
+++ lib/diplomat/datacenter.rb
@@ -0,0 +1,27 @@
+require 'base64'
+require 'faraday'
+
+module Diplomat
+  class Datacenter < Diplomat::RestClient
+
+    @access_methods = [ :get ]
+
+    # Get an array of all avaliable datacenters accessible by the local consul agent
+    # @param meta [Hash] output structure containing header information about the request (index)
+    # @return [OpenStruct] all datacenters avaliable to this consul agent
+    def get meta=nil
+
+      url = ["/v1/catalog/datacenters"]
+
+      ret = @conn.get concat_url url
+
+      if meta and ret.headers
+        meta[:index] = ret.headers["x-consul-index"]
+        meta[:knownleader] = ret.headers["x-consul-knownleader"]
+        meta[:lastcontact] = ret.headers["x-consul-lastcontact"]
+      end
+      return JSON.parse(ret.body)
+    end
+
+  end
+end
