4fdfe0a3ec048f6b1383b8b5a1901e72695b0969
Sam Marx <smarx@spredfast.com>
2016-01-27 11:25:38 -0600

    Add datacenter option to health

--- lib/diplomat/health.rb
+++ lib/diplomat/health.rb
@@ -8,34 +8,74 @@ module Diplomat
 
     # Get node health
     # @param n [String] the node
+    # @param options [Hash] :dc string for dc specific query
     # @return [OpenStruct] all data associated with the node
-    def node n
-      ret = @conn.get "/v1/health/node/#{n}"
+    def node n, options=nil
+      url = ["/v1/health/node/#{n}"]
+      url << use_named_parameter('dc', options[:dc]) if options and options[:dc]
+
+      # If the request fails, it's probably due to a bad path
+      # so return a PathNotFound error.
+      begin
+        ret = @conn.get concat_url url
+      rescue Faraday::ClientError
+        raise Diplomat::PathNotFound
+      end
       return JSON.parse(ret.body)
     end
 
     # Get service checks
     # @param s [String] the service
+    # @param options [Hash] :dc string for dc specific query
     # @return [OpenStruct] all data associated with the node
-    def checks s
-      ret = @conn.get "/v1/health/checks/#{s}"
+    def checks s, options=nil
+      url = ["/v1/health/checks/#{s}"]
+      url << use_named_parameter('dc', options[:dc]) if options and options[:dc]
+      
+      # If the request fails, it's probably due to a bad path
+      # so return a PathNotFound error.
+      begin
+        ret = @conn.get concat_url url
+      rescue Faraday::ClientError
+        raise Diplomat::PathNotFound
+      end
       return JSON.parse(ret.body)
     end
 
     # Get service health
     # @param s [String] the service
+    # @param options [Hash] :dc string for dc specific query
     # @return [OpenStruct] all data associated with the node
-    def service s
-      ret = @conn.get "/v1/health/service/#{s}"
+    def service s, options=nil
+      url = ["/v1/health/service/#{s}"]
+      url << use_named_parameter('dc', options[:dc]) if options and options[:dc]
+
+      # If the request fails, it's probably due to a bad path
+      # so return a PathNotFound error. 
+      begin
+        ret = @conn.get concat_url url
+      rescue Faraday::ClientError
+        raise Diplomat::PathNotFound
+      end
       return JSON.parse(ret.body)
     end
 
     # Get service health
     # @param s [String] the state ("unknown", "passing", "warning", or "critical")
     # @return [OpenStruct] all data associated with the node
-    def state s
-      ret = @conn.get "/v1/health/state/#{s}"
-      return JSON.parse(ret.body)
+    def state s, options=nil
+       url = ["/v1/health/state/#{s}"]
+       url << use_named_parameter('dc', options[:dc]) if options and options[:dc]
+       
+       # If the request fails, it's probably due to a bad path
+       # so return a PathNotFound error.
+       begin
+         ret = @conn.get concat_url url
+       rescue Faraday::ClientError
+         raise Diplomat::PathNotFound
+       end
+       return JSON.parse(ret.body)
+
     end
 
     # Convenience method to get services in unknown state
