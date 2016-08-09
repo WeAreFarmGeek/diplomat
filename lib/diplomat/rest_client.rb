981eb5f2089e58987d9312ab530f6e310430a482
Stefan Merettig <stefan-merettig@nuriaproject.org>
2016-08-09 20:45:51 +0200

    Add .respond_to? and .respond_to_missing? to Diplomat::RestClient (#68)
    * Add .respond_to? and .respond_to_missing? to Diplomat::RestClient
    
    * Fix whacky RestClient.method_missing method raising NameError
    
    See https://bugs.ruby-lang.org/issues/10969 for the ruby bug report

--- lib/diplomat/rest_client.rb
+++ lib/diplomat/rest_client.rb
@@ -43,9 +43,35 @@ module Diplomat
       # @param *args Arguments list
       # @return [Boolean]
       def method_missing(meth_id, *args)
-        access_method?(meth_id) ? new.send(meth_id, *args) : super
+        if access_method?(meth_id)
+          new.send(meth_id, *args)
+        else
+
+          # See https://bugs.ruby-lang.org/issues/10969
+          begin
+            super
+          rescue NameError => err
+            raise NoMethodError, err
+          end
+        end
       end
 
+      # Make `respond_to?` aware of method short-cuts.
+      #
+      # @param meth_id [Symbol] the tested method
+      # @oaram with_private if private methods should be tested too
+      def respond_to?(meth_id, with_private = false)
+        access_method?(meth_id) || super
+      end
+
+      # Make `respond_to_missing` aware of method short-cuts. This is needed for
+      # {#method} to work on these, which is helpful for testing purposes.
+      #
+      # @param meth_id [Symbol] the tested method
+      # @oaram with_private if private methods should be tested too
+      def respond_to_missing?(meth_id, with_private = false)
+        access_method?(meth_id) || super
+      end
     end
 
     private
