0ad0fcec1f17f2fa31ad4a9f4e3a56f085790416
Pierre Cheynier <p.cheynier@criteo.com>
2016-12-13 13:30:07 +0100

    Add prepared queries support

--- lib/diplomat/error.rb
+++ lib/diplomat/error.rb
@@ -4,6 +4,8 @@ module Diplomat
   class KeyAlreadyExists < StandardError; end
   class AclNotFound < StandardError; end
   class AclAlreadyExists < StandardError; end
+  class PreparedQueryNotFound < StandardError; end
+  class PreparedQueryAlreadyExists < StandardError; end
   class EventNotFound < StandardError; end
   class EventAlreadyExists < StandardError; end
   class UnknownStatus < StandardError; end
