1abb81300139380cdcb2d7d8de8371465b23c0be
Eugen Mayer <eugen.mayer@kontextwork.de>
2017-07-21 17:31:58 +0200

    Do not set the adapter on faraday prior the request
    Related to https://github.com/lostisland/faraday/pull/685 and fixes #148

--- lib/diplomat/rest_client.rb
+++ lib/diplomat/rest_client.rb
@@ -81,8 +81,8 @@ module Diplomat
 
     def build_connection(api_connection, raise_error = false)
       api_connection || Faraday.new(Diplomat.configuration.url, Diplomat.configuration.options) do |faraday|
-        faraday.adapter  Faraday.default_adapter
         faraday.request  :url_encoded
+        faraday.adapter  Faraday.default_adapter
         faraday.response :raise_error unless raise_error
 
         Diplomat.configuration.middleware.each do |middleware|
