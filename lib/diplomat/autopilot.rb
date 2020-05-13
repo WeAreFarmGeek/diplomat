95a39d56a5846ccd788095f32cdf3694bb19ed81
Ryan Hughes <ryan.hughes@digitalonus.com>
2020-05-13 13:52:00 -0400

    Adding in custom parameters

--- lib/diplomat/autopilot.rb
+++ lib/diplomat/autopilot.rb
@@ -9,6 +9,10 @@ module Diplomat
     # @param options [Hash] options parameter hash
     # @return [OpenStruct] all data associated with the autopilot configuration
     def get_configuration(options = {})
+      custom_params = []
+      custom_params << use_named_parameter('dc', options[:dc]) if options[:dc]
+      custom_params << use_named_parameter('stale', options[:dc]) if options[:stale]
+
       ret = send_get_request(@conn, ['/v1/operator/autopilot/configuration'], options, custom_params)
       JSON.parse(ret.body)
     end
@@ -17,6 +21,9 @@ module Diplomat
     # @param options [Hash] options parameter hash
     # @return [OpenStruct] all data associated with the health of the autopilot
     def get_health(options = {})
+      custom_params = []
+      custom_params << use_named_parameter('dc', options[:dc]) if options[:dc]
+
       ret = send_get_request(@conn, ['/v1/operator/autopilot/health'], options, custom_params)
       JSON.parse(ret.body)
     end
