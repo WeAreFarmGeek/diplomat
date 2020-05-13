dc42b3474bc5231c7d42f1b0a65d003e51307339
Ryan Hughes <ryan.hughes@digitalonus.com>
2020-05-13 09:21:23 -0400

    Initial support for the autopilot endpoint

---
+++ lib/diplomat/autopilot.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Diplomat
+    # Methods for interacting with the Consul operator autopilot API endpoint
+    class Nodes < Diplomat::RestClient
+      @access_methods = %i[get_configuration get_health update]
+  
+      # Get autopilot configuration
+      # @param options [Hash] options parameter hash
+      # @return [OpenStruct] all data associated with the autopilot configuration
+      def get_configuration(options = {})
+        ret = send_get_request(@conn, ['/v1/operator/autopilot/configuration'], options, custom_params)
+        JSON.parse(ret.body)
+      end
+      
+      # Get health status from the autopilot
+      # @param options [Hash] options parameter hash
+      # @return [OpenStruct] all data associated with the health of the autopilot
+      def get_health(options = {})
+        ret = send_get_request(@conn, ['/v1/operator/autopilot/health'], options, custom_params)
+        JSON.parse(ret.body)
+      end
+    end
+  end
+  
